899817248bae6e9664f8e84ff35b7e664c6c862e
rcassani <raymundo.cassani@gmail.com>
2022-08-19 09:22:49 -0400

    Update E01: Distance in meters and readme file

---
+++ E01/README.md
@@ -0,0 +1,4 @@
+# Result `file_coords_to_geom.py`
+```
+The average Euclidian distance between points was: 14.10 km
+```
--- E01/file_coords_to_geom.py
+++ E01/file_coords_to_geom.py
@@ -7,7 +7,18 @@ Reading coordinates from a CSV file and creating shapefile geometries
 import csv
 import numpy as np
 from shapely.geometry import Point, LineString
+from shapely.ops import transform
+import pyproj
 
+# Coordinate Reference System
+epsg_4326 = pyproj.CRS('EPSG:4326')
+epsg_3035 = pyproj.CRS('EPSG:3035')
+# EPSG:3035 = Lambert Azimuthal Equal Area projection (LAEA),
+# recommended projection by European Comission.
+# Projection
+project = pyproj.Transformer.from_crs(epsg_4326, epsg_3035, always_xy=True).transform
+
+# Origin and Destination points are given in degrees (EPSG:4326 aka WGS84)
 # Read file as CSV but ; rather than ,
 items = []
 with open('../data/travelTimes_2015_Helsinki.txt') as fin:
@@ -20,25 +31,19 @@ with open('../data/travelTimes_2015_Helsinki.txt') as fin:
       items.append(item)
 
 # Create lists of points and lines
-orig_points = []
-dest_points = []
-lines = []
 distances = []
 
+# Create geometries for each trip
 for item in items:
-   orig_point = Point(float(item['from_x']), float(item['from_y'])) 
-   orig_points.append(orig_point)
-   dest_point = Point(float(item['to_x']), float(item['to_y']))
-   dest_points.append(dest_point)
-   line = LineString([orig_point, dest_point]) 
-   lines.append(line)
+   # Origin and Destination points are given in degrees (EPSG:4326 aka WGS84)
+   orig_point_deg = Point(float(item['from_x']), float(item['from_y']))
+   dest_point_deg = Point(float(item['to_x']), float(item['to_y']))
+   # Project Points to have them in meters
+   orig_point_m = transform(project, orig_point_deg)
+   dest_point_m = transform(project, dest_point_deg)
+   line = LineString([orig_point_m, dest_point_m])
    distances.append(line.length)
    
 distances = np.array(distances) # in meters
 mean_dist = np.mean(distances)
-print('The mean Euclidian (x-y plane) distance was: {0} degrees'.format(mean_dist))
-print('These are NOT degrees for the "great-circle distance"')
-# To have the units in meters, this has to be projected to a
-# Spatial Reference System (SRS) or 
-# Coordinate Reference System (CRS) or
-# Coordiante System (CS)
+print('The average Euclidian distance between points was: {0:.2f} km'.format(mean_dist/1000))
